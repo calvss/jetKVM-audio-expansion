5fed618ea525eb2e52a24a830e9e8251d2459e33
calvss <calvin.alexander.ng@gmail.com>
2025-02-25 15:47:55 -0500

    First try DMA

--- kvm-audio-sw/kvm-audio-sw.cpp
+++ kvm-audio-sw/kvm-audio-sw.cpp
@@ -5,55 +5,82 @@
 
 #include "hardware/adc.h"
 #include "hardware/uart.h"
+#include "hardware/dma.h"
 
-#define ADC_CHANNEL 0
+#define ADC_CHANNEL_0 0
+#define ADC_CHANNEL_1 1
+#define CAPTURE_DEPTH 200'000
 
-volatile uint16_t buffer[UINT8_MAX+1];
-volatile bool readFlag = false;
-uint8_t i = 0;
-uint32_t count = 0;
-
-bool timer_callback(repeating_timer_t *rt);
+volatile uint16_t buffer[CAPTURE_DEPTH];
 
 int main()
 {
     stdio_init_all();
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, false);
+    sleep_ms(3000);
+    gpio_put(PICO_DEFAULT_LED_PIN, true);
 
-    int speed = uart_init(uart0, 3000000);
+    printf("Startup\n");
+    sleep_ms(10000);
+    uart_init(uart0, 3'000'000);
 
-    repeating_timer_t timer;
-
-    if (!add_repeating_timer_us(-1000000, timer_callback, NULL, &timer)) {
-        printf("Failed to add timer\n");
-        return 1;
-    }
-
-
+    adc_gpio_init(26 + ADC_CHANNEL_0);
+    adc_gpio_init(26 + ADC_CHANNEL_1);
 
     adc_init();
 
-    adc_gpio_init(26 + ADC_CHANNEL);
-    adc_select_input(ADC_CHANNEL);
+    // bit mask for first 2 ADC channels
+    adc_set_round_robin(0b00000011);
 
-    while (1) {
-        buffer[i] = adc_read();
-        i++;
-        count++;
+    // switch channel to 0 explicitly so the round robin always runs 0-1-0-1... instead of 1-0-1-0...
+    adc_select_input(ADC_CHANNEL_0);
 
-        if(readFlag)
-        {
-            printf("%lu vscode!\n", count);
-            count = 0;
-            readFlag = false;
-        }
+    adc_fifo_setup(
+        true,    // Write each completed conversion to the sample FIFO
+        true,    // Enable DMA data request (DREQ)
+        1,       // DREQ (and IRQ) asserted when at least 1 sample present
+        false,   // Disable the ERR bit
+        false    // Don't shift each sample to 8 bits when pushing to FIFO
+    );
+
+    // adc_set_clkdiv(0);
+    // set clock to 40 kHz (1199 + 1 clock cycles at 48MHz = 25 microseconds)
+    adc_set_clkdiv(599);
+
+    unsigned int dma_channel = dma_claim_unused_channel(true);
+    dma_channel_config cfg = dma_channel_get_default_config(dma_channel);
+
+    // Reading from constant address, writing to incrementing byte addresses
+    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
+    channel_config_set_read_increment(&cfg, false);
+    channel_config_set_write_increment(&cfg, true);
+
+    // Pace transfers based on availability of ADC samples
+    channel_config_set_dreq(&cfg, DREQ_ADC);
+
+    dma_channel_configure(dma_channel, &cfg,
+        buffer,         // dst
+        &adc_hw->fifo,  // src
+        CAPTURE_DEPTH,  // transfer count
+        true            // start immediately
+    );
+
+    sleep_ms(1000);
+    printf("Starting capture\n");
+
+    adc_run(true);
+
+    dma_channel_wait_for_finish_blocking(dma_channel);
+    printf("Capture finished\n");
+    adc_run(false);
+    adc_fifo_drain();
+
+    // Print samples to stdout so you can display them in pyplot, excel, matlab
+    for (int i = 0, j = 0; i < CAPTURE_DEPTH; i += 2, j++) {
+        printf("%d, %d, %d\n", j, buffer[i], buffer[i+1]);
     }
 
     return 0;
 }
-
-bool timer_callback(__unused repeating_timer_t* rt)
-{
-    readFlag = true;
-    return true;
-}
-
